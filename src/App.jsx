ac2c40ca1c4ff84aea256cc1f3913db6b94e719e
Shubha0707 <125174504+Shubha0707@users.noreply.github.com>
2025-09-10 19:29:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from './Components/hero'
+import Hero from './Components/Hero'
 import About from './Components/About'
 import Navbar from './Components/Navbar'
 import Features from './Components/Features'
